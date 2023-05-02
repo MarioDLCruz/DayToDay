f5f874d1f75a662d3ae205d87d7df50b9ba782ff
MarioDLCruz <125337040+MarioDLCruz@users.noreply.github.com>
2023-05-02 12:55:18 -0600

    Initial commit

---
+++ app/src/main/java/com/example/daytoday/cuestinario_registro_1.java
@@ -0,0 +1,14 @@
+package com.example.daytoday;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cuestinario_registro_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instrucciones);
+    }
+}
